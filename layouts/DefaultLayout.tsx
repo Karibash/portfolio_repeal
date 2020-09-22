93741e969b14d7bc134c1faa7d1eaa50ab057343
Karibash <y.karibayashi@gmail.com>
2020-09-22 22:23:26 +0900

    Fix the background position to be specified explicitly

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -11,6 +11,10 @@ const Wrapper = styled.div`
 
 const FixedBackground = styled(ParallaxParticles)`
   position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   width: 100%;
   height: 100%;
   min-width: 100vw;
