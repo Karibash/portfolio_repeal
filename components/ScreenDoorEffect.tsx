9b1c27a344106c94519f89208b75add04e23da54
Karibash <y.karibayashi@gmail.com>
2020-10-02 22:56:31 +0900

    Change to define the style inside the function component definition

--- components/ScreenDoorEffect.tsx
+++ components/ScreenDoorEffect.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const Effect = styled.div`
-  background: linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%), linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06));
-  background-size: 100% 2px, 3px 100%;
-  pointer-events: none;
-`;
-
 type Props = {
   readonly className?: string;
 };
 const ScreenDoorEffect: React.FC<Props> = ({
   className = '',
 }) => {
+  const Effect = styled.div`
+    background: linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%),
+                linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06));
+    background-size: 100% 2px, 3px 100%;
+    pointer-events: none;
+  `;
+
   return (
     <Effect className={className} />
   );
--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,43 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import styled from '@emotion/styled';
+import { useTheme } from "@emotion/react";
 
 import ParallaxParticles from "components/ParallaxParticles";
 import ScreenDoorEffect from "components/ScreenDoorEffect";
-
-const Wrapper = styled.div`
-  color: ${props => props.theme.color.foreground[500].string()};
-  background-color: ${props => props.theme.color.background[500].string()};
-`;
-
-const Main = styled.main`
-  min-width: 100vw;
-  min-height: 100vh;
-`;
-
-const FixedBackground = styled(ParallaxParticles)`
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
-  min-width: 100vw;
-  min-height: 100vh;
-`;
-
-const FixedScreenDoorEffect = styled(ScreenDoorEffect)`
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  height: 100%;
-  min-width: 100vw;
-  min-height: 100vh;
-`;
 
 const DefaultLayout: React.FC<{
   readonly title?: string;
@@ -45,6 +12,42 @@ const DefaultLayout: React.FC<{
   children,
   title = 'Karibash | Portfolio',
 }) => {
+  const theme = useTheme();
+
+  const Wrapper = styled.div`
+    color: ${theme.color.foreground[500].string()};
+    background-color: ${theme.color.background[500].string()};
+  `;
+
+  const Main = styled.main`
+    min-width: 100vw;
+    min-height: 100vh;
+  `;
+
+  const FixedBackground = styled(ParallaxParticles)`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    min-width: 100vw;
+    min-height: 100vh;
+  `;
+
+  const FixedScreenDoorEffect = styled(ScreenDoorEffect)`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    min-width: 100vw;
+    min-height: 100vh;
+  `;
+
   return (
     <Wrapper>
       <Head>
