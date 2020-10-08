de928062931a25c2fe817fdaa67ae501e526bd8e
Karibash <y.karibayashi@gmail.com>
2020-10-08 23:36:19 +0900

    Make the useMediaQuery function more generic

--- hooks/useMediaQuery.tsx
+++ hooks/useMediaQuery.tsx
@@ -2,24 +2,62 @@ import { useMedia } from 'react-use';
 
 import { useIsClient } from 'hooks/useIsClient';
 
-export const BreakPoint = {
-  small: 640,
-  medium: 768,
-  large: 1024,
-  huge: 1280,
+interface Query {
+  readonly string: string;
+}
+
+export const CombineCondition = {
+  And: 'and',
+  Or: ',',
 } as const;
-export type BreakPoint = typeof BreakPoint[keyof typeof BreakPoint];
+export type CombineCondition = typeof CombineCondition[keyof typeof CombineCondition];
 
-export const WidthCriteria = {
-  max: 'max-width',
-  min: 'min-width',
+export class CombineQuery implements Query {
+  private readonly condition: CombineCondition;
+  private readonly query1: Query;
+  private readonly query2: Query;
+
+  constructor(condition: CombineCondition, query1: Query, query2: Query) {
+    this.condition = condition;
+    this.query1 = query1;
+    this.query2 = query2;
+  }
+
+  get string() {
+    return `${this.query1.string} ${this.condition} ${this.query2.string}`;
+  }
+}
+
+export const WideDirection = {
+  Width: 'width',
+  Height: 'height',
 } as const;
-export type WidthCriteria = typeof WidthCriteria[keyof typeof WidthCriteria];
+export type WideDirection = typeof WideDirection[keyof typeof WideDirection];
 
-export const useMediaQuery = (breakpoint: BreakPoint, criteria: WidthCriteria): boolean => {
-  const emSize = breakpoint / 16;
-  const result = useMedia(`(${criteria}: ${emSize}em)`);
+export const SizeCondition = {
+  Max: 'max',
+  Min: 'min',
+} as const;
+export type SizeCondition = typeof SizeCondition[keyof typeof SizeCondition];
 
+export class SizeQuery implements Query {
+  private readonly emSize: number;
+  private readonly wideDirection: WideDirection;
+  private readonly sizeCondition: SizeCondition;
+
+  constructor(pixelSize: number, wideDirection: WideDirection, sizeCondition: SizeCondition) {
+    this.emSize = pixelSize / 16;
+    this.wideDirection = wideDirection;
+    this.sizeCondition = sizeCondition;
+  }
+
+  get string() {
+    return `(${this.sizeCondition}-${this.wideDirection}: ${this.emSize}em)`;
+  }
+}
+
+export const useMediaQuery = (query: Query): boolean => {
+  const matches = useMedia(query.string);
   const isClient = useIsClient();
-  return isClient ? result : false;
+  return isClient ? matches : false;
 };
