3287edcdff26747d9ed2bb7abf1b8c334f030072
Karibash <y.karibayashi@gmail.com>
2020-10-14 00:20:16 +0900

    Fix it to use useIsClient when judging the client in useWindowSize

--- hooks/useWindowSize.tsx
+++ hooks/useWindowSize.tsx
@@ -1,23 +1,27 @@
 import { useState, useEffect } from 'react';
 
+import { useIsClient } from 'hooks/useIsClient';
+
 export const useWindowSize = () => {
+  const isClient = useIsClient();
   const [windowSize, setWindowSize] = useState({
     width: 0,
     height: 0,
   });
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const handleResize = () => {
-        setWindowSize({
-          width: window.innerWidth,
-          height: window.innerHeight,
-        });
-      };
-      window.addEventListener("resize", handleResize);
-      handleResize();
-      return () => window.removeEventListener("resize", handleResize);
+    if (!isClient) {
+      return () => {};
     }
-  }, []);
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isClient]);
   return windowSize;
 }
