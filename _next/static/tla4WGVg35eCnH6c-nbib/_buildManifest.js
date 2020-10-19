09780494058a601f6d2ce48e63042e70807ac592
Karibash <Karibash@users.noreply.github.com>
2020-10-19 12:37:23 +0000

    deploy: cec6ff06336f495f57f2c54e68be458e053be222

--- _next/static/tla4WGVg35eCnH6c-nbib/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F29107295.95e34a61f32a1beaf42e.js","static\u002Fchunks\u002Fpages\u002Findex-7e74375ae3b418ffd241.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5efcade778114a255845.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
